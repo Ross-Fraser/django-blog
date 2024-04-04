16d701eeb640e25b5be5729a7e85bfb578289b77
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-04-04 10:50:17 +0100

    create: added fields to the dadabase

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-xh2=0^(^-uw=o(_!$5%#8!m-gbfjp)a80es&u68qi%(mk8ae7&
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
